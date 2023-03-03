3122d3dba5e6fa7937231893a7d01597cbbff403
tkaMymas <JuanesvanegasGallego@gmail.com>
2023-03-03 09:33:33 -0500

    Complete

--- Apartados/depositar.py
+++ Apartados/depositar.py
@@ -16,15 +16,21 @@ def depositarDinero():
         if deposito == 10 or deposito == 25 or deposito == 50 or deposito == 100:
             print("Ingrese la cantidad de montos de dinero depositara.")
             depositoMonto = int(input())
+            time.sleep(0.5)
             if depositoMonto < 10:
                 print("Procesando...")
-                time.sleep(0.5)
+                time.sleep(1)
 
                 totalIngreso = deposito * depositoMonto
                 dineroTotal += totalIngreso
                 time.sleep(1)
                 print("Transacción completada.. \nSe han ingresado la cantidad de", str(totalIngreso) + "$",
                       "a tu cuenta bancaria.")
+            else:
+                print("Procesando...")
+                print("Ingrese un monto por debajo de 10.")
+                print()
+                return depositarDinero()
             break
         else:
             time.sleep(0.5)
